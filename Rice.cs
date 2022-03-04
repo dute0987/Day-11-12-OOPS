3a0c5a3bc85788e7f50279431e9187116a662d84
dute0987 <dute0987@gmail.com>
2022-03-04 21:45:20 +0530

    rice file for class managment

---
+++ Rice.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventaroryDataManagmentDemo
+{
+    public class Rice
+    {
+        public List<TypeofRice> typeofRice;
+        public List<TypeofPules> typeofPules;
+        public List<TypeofWheats> typeofWheats;
+    }
+    public class TypeofRice
+    {
+        public string name;
+        public string weight;
+        public int price;
+
+    }
+    public class TypeofPules
+    {
+        public string name;
+        public string weight;
+        public int price;
+
+    }
+    public class TypeofWheats
+    {
+        public string name;
+        public string weight;
+        public int price;
+    }
+}
