bf31eb24b07c8c5f6461923ec47d43a797f3364e
dute0987 <dute0987@gmail.com>
2022-03-04 21:46:43 +0530

    file for fetching the data

---
+++ FetchJasonForRice.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventaroryDataManagmentDemo
+{
+    public class FetchJasonForRice
+    {
+       public Rice Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Problem in reading the file");
+                    return null;
+                }
+            }
+       }
+
+    }
+}
