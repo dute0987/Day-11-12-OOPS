81f9ab33cd27274cda21b24eadc9991a4da067bd
dute0987 <dute0987@gmail.com>
2022-03-10 18:32:20 +0530

    Vishal Dute-Main file for Stock Inventary

---
+++ Stock.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventaoryDataForStockCompany
+{
+    public class Stock
+    {
+        public List<BSE> BombaySE;
+        public List<NSE> NationalSE;
+    }
+    public class BSE
+    {
+        public string ShareName;
+        public int ShareNumber;
+        public int SharePrice;
+    }
+    public class NSE
+    {
+        public string ShareName;
+        public int ShareNumber;
+        public int SharePrice;
+    }
+}
