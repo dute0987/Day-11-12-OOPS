91ddedceac0bbe7e9e2349a3ddaabf994ef4517e
dute0987 <dute0987@gmail.com>
2022-03-10 18:31:49 +0530

    Vishal Dute-Fetch file for Stock Inventary

---
+++ FetchJsonForstock.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventaoryDataForStockCompany
+{
+    public class FetchJsonForstock
+    {
+       
+          public Stock Read(string path)
+          {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Stock>(json);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Problem in reading file");
+                    return null;
+                }
+            }
+          }
+    }
+}
